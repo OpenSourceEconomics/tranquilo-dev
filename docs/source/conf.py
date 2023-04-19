00b345ff95fbe03fd6dd3145e3060a5b97e44054
Sebastian Gsell <sebastian.gsell93@gmail.com>
2023-04-19 11:35:56 +0200

    Run sphinx-quickstart

---
+++ docs/source/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "tranquilo-dev"
+author = "Janos Gabler, Tim Mensinger, Sebastian Gsell and Mariam Petrosyan"
+release = "2023"
+copyright = "2023, {author}"  # noqa: A001
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
