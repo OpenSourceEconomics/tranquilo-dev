38340107143189616103344c426201aff66abbb6
mpetrosian <mano.act.g@gmail.com>
2023-04-24 12:28:09 +0200

    add line settings for parallelization_ls plots

--- src/tranquilo_dev/plotting/task_create_benchmark_plots.py
+++ src/tranquilo_dev/plotting/task_create_benchmark_plots.py
@@ -9,6 +9,32 @@ from tranquilo_dev.config import PLOT_CONFIG
 from tranquilo_dev.config import PROBLEM_SETS
 
 
+LINE_SETTINGS = {"parallelization_ls": {}, "noisy_ls": {}, "scalar_and_ls": {}}
+
+tranquilo_scenarios = [
+    sc for sc in PLOT_CONFIG["parallelization_ls"]["scenarios"] if "tranquilo" in sc
+]
+tranquilo_scenarios = sorted(tranquilo_scenarios, key=lambda x: x.split("_")[-1])
+
+competitor = [
+    sc
+    for sc in PLOT_CONFIG["parallelization_ls"]["scenarios"]
+    if sc not in tranquilo_scenarios
+]
+LINE_SETTINGS["parallelization_ls"][competitor] = {
+    "line": {"color": "red", "dash": "solid"},
+}
+
+
+alphas = [0.5, 0.8, 1]
+
+for i, scenario in enumerate(tranquilo_scenarios):
+    LINE_SETTINGS["parallelization_ls"][scenario] = {
+        "line": {"color": "blue", "dash": "solid"},
+        "opacity": alphas[i],
+    }
+
+
 for name, info in PLOT_CONFIG.items():
     problem_name = info["problem_name"]
     DEPS = {}
@@ -23,7 +49,7 @@ for name, info in PLOT_CONFIG.items():
         @pytask.mark.produces(OUT / f"{name}.svg")
         @pytask.mark.task(id=f"{plot_type}_plot_{name}")
         def task_create_benchmark_plots(
-            depends_on, produces, info=info, plot_type=plot_type
+            depends_on, produces, info=info, plot_type=plot_type, name=name
         ):
             results = {}
             for path in depends_on.values():
@@ -45,5 +71,9 @@ for name, info in PLOT_CONFIG.items():
                 results=results,
                 **kwargs,
             )
-
+            if plot_type == "profile":
+                for trace_name, kwargs in LINE_SETTINGS[name].items():
+                    for trace in fig.data:
+                        if trace.name == trace_name:
+                            trace.update(kwargs)
             fig.write_image(produces)
